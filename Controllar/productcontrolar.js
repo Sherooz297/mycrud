073d19f9a8620e4a3b85d8473827a7f6146b37c7
Aimal Sherooz <aimalsherooz123@gmail.com>
2023-06-29 12:36:27 +0500

    update product and get single products API is completed and Backend is also completed

--- Controllar/productcontrolar.js
+++ Controllar/productcontrolar.js
@@ -3,31 +3,31 @@ const Product = require("../Model/productModal")
 
 //Creating the new product..
 
-exports.createproduct = async(req,res,next) =>{
-try {
-    const  {
-        name,
-        description,
-        price
-    } = req.body
+exports.createproduct = async (req, res, next) => {
+    try {
+        const {
+            name,
+            description,
+            price
+        } = req.body
 
-    const product = await Product.create({
-        name,
-        description,
-        price
-    })
-    res.status(200).json({
-        success : true,
-        product
+        const product = await Product.create({
+            name,
+            description,
+            price
+        })
+        res.status(200).json({
+            success: true,
+            product
 
-    })
-    
-} catch (error) {
-    res.status(400).json({
-        success:false,
-        message:error.message
-    }) 
-}
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: error.message
+        })
+    }
 
 }
 
@@ -35,52 +35,102 @@ try {
 //GET the all products using GET request
 
 
-exports.getproduct = async(req,res,next) => {
+exports.getproduct = async (req, res, next) => {
 
     try {
-        const product =await Product.find()
-        if(!product){
-          return  res.status(404).json({error:"no product found"})
+        const product = await Product.find()
+        if (!product) {
+            return res.status(404).json({ error: "no product found" })
         }
-    
+
         res.status(200).json({
-            success:true,
+            success: true,
             product
         })
-        
+
     } catch (error) {
         res.status(400).json({
-            success:false,
-            message:error.message
-        }) 
+            success: false,
+            message: error.message
+        })
     }
 
 }
+
+
+//GET single product using GET api
+exports.getsingleproduct = async(req,res,next) =>{
+
+    try {
+        const singleuser = await Product.findById(req.params.id)
+        if(!singleuser){
+            res.status(400).json({
+                error:"user not found",
+                
+            })
+        }
+        res.status(200).json({
+            success:true,
+            singleuser
+        })
+    } catch (error) {
+
+         res.status(404).json({ error: "internal server error" })
+    }
+
+ 
+} 
+
 
 
 // DELETE the product using DELETE request
 
-exports.deleteproduct = async(req,res,next) =>{
+exports.deleteproduct = async (req, res, next) => {
 
     try {
         const product = await Product.findByIdAndDelete(req.params.id);
-        if(!product){
-           return res.status(404).json({error:"not found"})
+        if (!product) {
+            return res.status(404).json({ error: "not found" })
         }
+
+        res.status(200).json({
+            success: true,
+            message: "delete done",
+
+        })
+
+
+    } catch (error) {
+        res.status(404).json({ error: "internal server error" })
+
+    }
+
+
+
+}
+
+
+//edit the product using PUT api
+
+exports.updateproduct = async (req, res, next) => {
+
+    try {
+        const { name,
+            description,
+            price } = req.body
+    
+        const updatedproduct = {name,description,price}
+    
+        await Product.findByIdAndUpdate(req.params.id,updatedproduct)
     
         res.status(200).json({
             success:true,
-            message:"delete done",
-            
+            message :"product updated",
         })
-    
         
     } catch (error) {
-        res.status(404).json({error:"internal server error"})
-        
+        res.status(404).json({ error: "internal server error" })
     }
 
- 
-
 }
- 
+
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,17 @@
 const express = require('express')
  const router = express.Router()
- const { createproduct, getproduct, deleteproduct } = require('../Controllar/productcontrolar')
+ const { createproduct, getproduct, deleteproduct, updateproduct, getsingleproduct } = require('../Controllar/productcontrolar')
  
 
  router.route("/Create-Product").post(createproduct)
 
  router.route("/Get-Product").get(getproduct) 
 
+ router.route("/Get-Single-Product/:id").get(getsingleproduct)
+
  router.route("/Delete-Product/:id").delete(deleteproduct)
+
+ router.route("/Update-Product/:id").put(updateproduct)
 
 
 
