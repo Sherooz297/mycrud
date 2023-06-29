eeaeb59ac1e1cde455d24bd945b05793b699d7e9
Aimal Sherooz <aimalsherooz123@gmail.com>
2023-06-29 10:35:33 +0500

    getting product and deleting product is completed

--- Controllar/productcontrolar.js
+++ Controllar/productcontrolar.js
@@ -30,3 +30,57 @@ try {
 }
 
 }
+
+
+//GET the all products using GET request
+
+
+exports.getproduct = async(req,res,next) => {
+
+    try {
+        const product =await Product.find()
+        if(!product){
+          return  res.status(404).json({error:"no product found"})
+        }
+    
+        res.status(200).json({
+            success:true,
+            product
+        })
+        
+    } catch (error) {
+        res.status(400).json({
+            success:false,
+            message:error.message
+        }) 
+    }
+
+}
+
+
+// DELETE the product using DELETE request
+
+exports.deleteproduct = async(req,res,next) =>{
+
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+        if(!product){
+           return res.status(404).json({error:"not found"})
+        }
+    
+        res.status(200).json({
+            success:true,
+            message:"delete done",
+            
+        })
+    
+        
+    } catch (error) {
+        res.status(404).json({error:"internal server error"})
+        
+    }
+
+ 
+
+}
+ 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express')
  const router = express.Router()
- const { createproduct } = require('../Controllar/productcontrolar')
+ const { createproduct, getproduct, deleteproduct } = require('../Controllar/productcontrolar')
  
 
  router.route("/Create-Product").post(createproduct)
+
+ router.route("/Get-Product").get(getproduct) 
+
+ router.route("/Delete-Product/:id").delete(deleteproduct)
 
 
 
